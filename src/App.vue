4213343b076975105fc653db54236a048054a0b1
Syahrul Bastomy <syahrulb13@gmail.com>
2020-09-06 19:11:55 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,12 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+    <router-view></router-view>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+<style >
+  @import "~animate.css/animate.css";
 </style>
+<style src="./assets/css/home-page/line-awesome.min.css"></style>
+<style src="./assets/css/home-page/all.min.css"></style>
+<style src="./assets/css/home-page/style.css"></style>
+<style src="./assets/css/home-page/responsive.css"></style>
+<style src="./assets/css/home-page/color.css"></style>
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
-
   const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    component: () => import('@/components'),
+    children: [
+        {
+            path: '/',
+            name: "Halaman-Utama",
+            meta : "front-end",
+            component: () => import('@/views/blog/index'),
+        },
+    ]
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
 ]
 
 const router = new VueRouter({
